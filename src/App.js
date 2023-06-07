3e00f6f84028b707fbd100a0d8c0be5f9cb9d643
Sergei Kholodenin <65288088+sergei-kholodenin@users.noreply.github.com>
2023-06-07 20:14:00 +0300

    create button component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' />
+      </Routes>
+    </Router>
   );
 }
 
