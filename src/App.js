bb6a857853f287b64e3e055215de4a66e840f963
Sergei Kholodenin <65288088+sergei-kholodenin@users.noreply.github.com>
2023-06-08 00:24:36 +0300

    add cards

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './Pages/Home';
+import Products from './Pages/Products';
+import Services from './Pages/Services';
+import SignUp from './Pages/SignUp';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Navbar/>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/products' element={<Products/>} />
+        <Route path='/services' element={<Services/>} />
+        <Route path='/sign-up' element={<SignUp/>} />
       </Routes>
     </Router>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection';
+import Cards from '../components/Cards';
 
 function Home() {
     return (
         <>
             <HeroSection/>
+            <Cards/>
         </>
     )
 }
