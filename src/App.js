91ff091a766df3906d5e0f410640ec5f001f48a1
Sergei Kholodenin <65288088+sergei-kholodenin@users.noreply.github.com>
2023-06-07 23:19:42 +0300

    create hero section and home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './Pages/Home';
 
 function App() {
   return (
     <Router>
       <Navbar/>
       <Routes>
-        <Route path='/' />
+        <Route path='/' element={<Home/>} />
       </Routes>
     </Router>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,31 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {Link} from 'react-router-dom';
+import { Button } from './Button';
+import './Navbar.css';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+    const showButton = () => {
+        if (window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true);
+        }
+    }
+
+    useEffect(() => {
+        showButton();
+    }, []);
+
+    window.addEventListener('resize', showButton);
     return (
         <>
         <nav className="navbar">
             <div className="navbar-container">
-                <Link to='/' className='navbar-logo'>
+                <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                     TRVL <i className="fab fa-typo3"></i>
                 </Link>
                 <div className="menu-icon" onClick={handleClick}>
@@ -29,6 +45,7 @@ function Navbar() {
                         <Link to='/sing-up' className="nav-links-mobile" onClick={closeMobileMenu}>Sing Up</Link>
                     </li>
                 </ul>
+                {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
             </div>
         </nav>
     </>
