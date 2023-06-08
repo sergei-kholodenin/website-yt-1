e05c58f5eca6179cec9707bd1e2713b99ab38f51
Sergei Kholodenin <65288088+sergei-kholodenin@users.noreply.github.com>
2023-06-08 21:56:06 +0300

    create footer

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroSection from '../components/HeroSection';
 import Cards from '../components/Cards';
+import Footer from '../components/Footer';
 
 function Home() {
     return (
         <>
             <HeroSection/>
             <Cards/>
+            <Footer/>
         </>
     )
 }
